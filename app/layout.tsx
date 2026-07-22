f7162453a8a0c3004f61ece409d3647e9cf4e5bb
Tomás Rasteiro <tomasrasteiro0@gmail.com>
2026-07-22 22:53:20 +0100

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,48 +2,43 @@ import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Cursor from "@/sections/Cursor";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-
 const playfair = Playfair_Display({
   subsets: ["latin"],
   variable: "--font-playfair",
 });
 
-
-export const metadata = {
+export const metadata: Metadata = {
   title: "Rasteiro Visuals",
   description: "Photography, Video Editing and Graphic Design",
   icons: {
     icon: "/logoTopoSite.png",
   },
+  verification: {
+    google: "yXe3_Hkv9yzQ6AWIOc0LR34SSDTYeT2msqUTRHsW-HQ",
+  },
 };
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
-
-      <body
-        className={`${inter.variable} ${playfair.variable}`}
-      >
-
+      <body className={`${inter.variable} ${playfair.variable}`}>
         <Cursor />
 
         {children}
 
+        <SpeedInsights />
       </body>
-
     </html>
   );
 }
